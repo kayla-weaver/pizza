d81751f67c88eb477dde6cc8b2e92c7f3ee19942
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-15 11:55:10 -0700

    add ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
+//business logic
 function Pizza (topping, size){
     this.topping = topping;
     this.size = size;
+}
+
+
+
+//ui logic
+window.addEventListener("load", function(){
+    const form = document.getElementById("toppingForm");
+    form.addEventListener("submit", displayToppings)
+})
+  function displayToppings(event){
+    event.preventDefault();
+    const selectedToppings = []
+    const toppingsList = document.getElementsByName("toppings");
+  toppingsList.forEach(function(toppings){
+    if (toppings.checked){
+      selectedToppings.push(toppings.value)
+    }
+  })
+  document.getElementById("result").innerText =`You selected these kinds of toppings: ${selectedToppings}`
 }
