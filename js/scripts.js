a522a8a655467eb0a12cf51888827df36f5e5546
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-15 18:16:46 -0700

    cost function working

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ function Pizza (veggies, meats, size){
     this.veggies = veggies;
     this.meats = meats;
     this.size = size;
-    console.log("Pizzaaaa")
+    console.log("Pizzaaaa");
 }
-let cost = {
-    price: ""
-    sayPrice: function(){
+let Cost = {
+    price: "",
+    sayPrice: function() {
         console.log("Your price today is" + "" + this.price);
     }
 }
-let pizzaCost = object.create(Cost);
-pizza.cost = 
-pizza.sayPrice();
+let pizzaCost = Object.create(Cost);
+pizzaCost.price = 
+pizzaCost.sayPrice();
 
 
 
@@ -22,14 +22,14 @@ window.addEventListener("load", function(){
     const form = document.getElementById("toppingForm");
     form.addEventListener("submit", displayToppings)
 })
-  function displayToppings(event){
+    function displayToppings(event){
     event.preventDefault();
     const selectedToppings = []
     const toppingsList = document.getElementsByName("toppings");
-  toppingsList.forEach(function(toppings){
+    toppingsList.forEach(function(toppings){
     if (toppings.checked){
-      selectedToppings.push(toppings.count)
-      console.log("toppings.checked", toppings.checked);
+    selectedToppings.push(toppings.count)
+    console.log("toppings.checked", toppings.checked);
     }
   })
   document.getElementById("result").innerText =`You selected these kinds of toppings: ${selectedToppings}`
