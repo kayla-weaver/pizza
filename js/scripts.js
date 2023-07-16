ebd2772ff7b701dabda8efdf0bdebc2c2e6374c9
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-15 21:57:44 -0700

    make changes to forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //business logic
-function Pizza (veggies, meats, size){
+function Pizza(veggies, meats, size) {
     this.veggies = veggies;
     this.meats = meats;
     this.size = size;
@@ -7,55 +7,59 @@ function Pizza (veggies, meats, size){
 }
 let Cost = {
     price: "",
-    sayPrice: function() {
+    sayPrice: function () {
         console.log("Your price today is" + "" + this.price);
-    }
-}
-let pizzaCost = Object.create(Cost);
-pizzaCost.price = 
-pizzaCost.sayPrice();
+    },
+};
 
-function veggiesTopping(){
-    var checkboxes= document.querySelectorAll(".veggieCheckbox");
+function veggiesTopping() {
+    const veggieList = ["artichoke", "bellpepper", "broccoli"];
+    var checkboxes = document.querySelectorAll(".veggieCheckbox");
     var count = 0;
-console.log("veggiesTopping");
-document.getElementById("toppingForm").onclick = function(){
-    console.log("Something like that");
-    for (var i = 0; i < checkboxes.length; i++){
-        checkboxes[i].addEventListener("click",function()  {
-            console.log("Adding event listener");
-            if (this.checked == true) {
-                console.log("chegged");
-                count++;
-            } else {
-                console.log("Not chegged");
-                count--;
-            }
-            document.getElementById("veggieCheckbox").innerHTML= count;
-        })
-    }
+    console.log("veggiesTopping");
+    document.getElementById("toppingForm").onclick = function () {
+        console.log("Something like that");
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].addEventListener("click", function () {
+                console.log("Adding event listener");
+                if (this.checked == true) {
+                    console.log("chegged");
+                    count++;
+                } else {
+                    console.log("Not chegged");
+                    count--;
+                }
+                document.getElementById("veggieCheckbox").innerHTML = count;
+            });
+        }
+    };
 }
-}
-
-
 
 //ui logic
-window.addEventListener("load", function(){
+function displayToppings(event) {
+    const selectedToppings = [];
+    const toppingsList = document.getElementsByName("toppings");
+    toppingsList.forEach(function (toppings) {
+        if (toppings.checked) {
+            selectedToppings.push(toppings.count);
+            console.log("toppings.checked", toppings.checked);
+        }
+    });
+    document.getElementById(
+        "result"
+    ).innerText = `You selected these kinds of toppings: ${selectedToppings}`;
+}
+
+function handleVeggieSubmission(event) {
+    event.preventDefault();
+    console.log("prevent default");
+    const veggiesTotalCost = veggiesTopping();
+}
+
+window.addEventListener("load", function () {
+    let pizzaCost = Object.create(Cost);
+    pizzaCost.price = pizzaCost.sayPrice();
     const form = document.getElementById("toppingForm");
     console.log(form);
-    form.addEventListener("submit", veggiesTopping)
-    form.addEventListener("submit", displayToppings)
-})
-function displayToppings(event){
-    event.preventDefault();
-
-    const selectedToppings = []
-    const toppingsList = document.getElementsByName("toppings");
-    toppingsList.forEach(function(toppings){
-    if (toppings.checked){
-    selectedToppings.push(toppings.count)
-    console.log("toppings.checked", toppings.checked);
-    }
-  })
-  document.getElementById("result").innerText =`You selected these kinds of toppings: ${selectedToppings}`
-  }
+    form.addEventListener("submit", handleVeggieSubmission);
+});
