bfe86a65318248285ac709ceb3629801cff481ff
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-21 23:02:06 -0700

    add totaled area to bottom

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ let Cost = {
 };
 
 Pizza.prototype.calculateCost = function(){
-    // all the logic to determine price
     let cost = 0;
     if (this.size === "tiny"){
         cost += 3
@@ -27,56 +26,17 @@ Pizza.prototype.calculateCost = function(){
         cost += 25
 
     cost += this.toppings.length;
-    this.price = cost;// whatever price has been calculated.
+    this.price = cost;
 };
 
 
 
 //ui logic
-// function displayToppings(event) {
-//     const selectedToppings = [];
-//     const toppingsList = document.getElementsByName("toppings");
-//     toppingsList.forEach(function (toppings) {
-//         if (toppings.checked) {
-//             selectedToppings.push(toppings.count);
-//             console.log("toppings.checked", toppings.checked);
-//         }
-//     });
-//     document.getElementById(
-//         "result"
-//     ).innerText = `You selected these kinds of toppings: ${selectedToppings}`;
-// }
-
-// function veggiesTopping() {
-//     const veggieList = ["artichoke", "bellpepper", "broccoli"];
-//     var checkboxes = document.querySelectorAll(".veggieCheckbox");
-//     var count = 0;
-//     console.log("veggiesTopping");
-//     document.getElementById("toppingForm").onclick = function () {
-//         console.log("Something like that");
-//         for (var i = 0; i < checkboxes.length; i++) {
-//             checkboxes[i].addEventListener("click", function () {
-//                 console.log("Adding event listener");
-//                 if (this.checked == true) {
-//                     console.log("chegged");
-//                     count++;
-//                 } else {
-//                     console.log("Not chegged");
-//                     count--;
-//                 }
-//                 document.getElementById("veggieCheckbox").innerHTML = count;
-//             });
-//         }
-//     };
-// }
 
 function handleVeggieSubmission(event) {
+    document.querySelector("div#price").removeAttribute("class");
     event.preventDefault();
-    // grab data from the forms
-    // const veggiesTotalCost = veggiesTopping();
-
     const pizzaSizes = document.getElementsByName("sizes")
-    console.log(pizzaSizes);
     let selectedSize = undefined;
     pizzaSizes.forEach(function(size){
         if (size.checked === true){
@@ -94,12 +54,9 @@ function handleVeggieSubmission(event) {
 
     let newPizza = new Pizza(selectedToppings, selectedSize)
     newPizza.calculateCost()
-    console.log(newPizza.price)
-    // display cost to user 
 }
 
 window.addEventListener("load", function () {
-    // get form, add event listener for when form submits
     const form = document.getElementById("toppingForm");
     form.addEventListener("submit", handleVeggieSubmission);
 });
